24d9919a52fa6ac0afae2446ee2bbbb79e357fc9
Tai <taing89@gmail.com>
2019-07-22 18:32:25 -0700

    Update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,19 +17,62 @@ var connection = mysql.createConnection({
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   showItems()
 });
 
 // Generates the items and shows it.
 function showItems() {
-  connection.query("SELECT * FROM products", function (err, res) {
-    for(var i=0; i<res.length; i++){
-      console.log(res[i].item_id+" --- "+res[i].product_name+" --- "+res[i].department_name+" --- "+res[i].price+" --- "+res[i].quantity+"\n");
+  connection.query("SELECT * FROM products", function (err, response) {
+    for (var i = 0; i < response.length; i++) {
+      console.log("Item #: " + response[i].item_id +
+        " --- Product Name: " + response[i].product_name +
+        " --- Department Name: " + response[i].department_name +
+        " --- Price: " + response[i].price +
+        " --- Quantity: " + response[i].quantity + "\n");
     }
-    connection.end();
-    start()
   });
 }
 
+var customerInput = function (response) {
+  inquirer.prompt([{
+    type: "input",
+    name: "choice",
+    message: "What would you like to purchase?"
+
+    // When customer gives an answer, it'll loop through the array and look for that item ID. If there is one, it'll say the response is true and set the input to item.
+  }]).then(function (answer) {
+    var correct = false;
+    for (var i = 0; i < response.length; i++) {
+      if (response[i].item_id == answer.choice) {
+        correct = true;
+        var item = answer.choice;
+        var id = i;
+
+        inquirer.prompt({
+          type: "input",
+          name: "quantity",
+          message: "How many do you want to buy?",
+          validate: function (value) {
+            if (isNaN(value) == false) {
+              return true;
+            } else {
+              return false;
+            }
+          }
+        }).then(function (answer) {
+          if ((response[id].quantity-answer.quantity) > 0) {
+            connection.query("UPDATE products SET quantity='" + (response[id].quantity-answer.quantity + "'WHERE item_ID'" + item + "'", function(err, response2) {
+              console.log("Item bought!");
+              makeTable();
+            })
+          } else {
+            console.log("Item is not a valid selection!");
+            promptCustomer(response);
+          }
+        })
+      }
+    }
+  })
+}
